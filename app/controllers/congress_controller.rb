3b8db679eacd13a26d10f3869e8a46613767e436
Erin Pintozzi <epintozzi@turing.edu>
2022-02-03 13:01:36 -0600

    refactor senate search

--- app/controllers/congress_controller.rb
+++ app/controllers/congress_controller.rb
@@ -34,17 +34,17 @@ class CongressController < ApplicationController
   end
 
   def search
-    conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
-      faraday.headers["X-API-KEY"] = ENV['govt_api_key']
-    end
-    response = conn.get("/congress/v1/116/senate/members.json")
+    @member = CongressFacade.senate_search(params[:search])
 
-    data = JSON.parse(response.body, symbolize_names: true)
-
-    members = data[:results][0][:members]
-
-    found_members = members.find_all {|m| m[:last_name] == params[:search]}
-    @member = found_members.first
+    # response = conn.get("/congress/v1/116/senate/members.json")
+    #
+    # data = JSON.parse(response.body, symbolize_names: true)
+    #
+    # members = data[:results][0][:members]
+    #
+    # found_members = members.find_all {|m| m[:last_name] == params[:search]}
+    # @member = found_members.first
+    # binding.pry
     render "welcome/index"
   end
 end
--- app/services/congress_service.rb
+++ app/services/congress_service.rb
@@ -10,6 +10,12 @@ class CongressService
     json = JSON.parse(response.body, symbolize_names: true)
   end
 
+  def self.senate
+    response = conn.get("/congress/v1/116/senate/members.json")
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def self.conn
     conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
       faraday.headers["X-API-KEY"] = ENV['govt_api_key']
