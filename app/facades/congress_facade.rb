44783b26c568c9ad6b10e49a60e5091697a3547f
Erin Pintozzi <epintozzi@turing.edu>
2022-03-01 12:42:20 -0600

    cleanup branch for caching lesson

--- app/facades/congress_facade.rb
+++ app/facades/congress_facade.rb
@@ -1,17 +1,7 @@
 class CongressFacade
   def self.members(state)
-    # state = params[:state]
-    # # Establishing our connection
-    # conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
-    #   faraday.headers["X-API-KEY"] = ENV['govt_api_key']
-    # end
-    #
-    # response = conn.get("/congress/v1/members/house/#{state}/current.json")
-    # # Parsing our response
-    # json = JSON.parse(response.body, symbolize_names: true)
     json = CongressService.members_of_house(state)
 
-    # Assigning response to object
     @members = json[:results].map do |member_data|
       Member.new(member_data)
     end
--- app/services/congress_service.rb
+++ app/services/congress_service.rb
@@ -1,12 +1,7 @@
 class CongressService
   def self.members_of_house(state)
-    # Establishing our connection
-    # conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
-    #   faraday.headers["X-API-KEY"] = ENV['govt_api_key']
-    # end
-
     response = conn.get("/congress/v1/members/house/#{state}/current.json")
-    # Parsing our response
+
     json = JSON.parse(response.body, symbolize_names: true)
   end
 
